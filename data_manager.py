3e811bc731d6bde65df7a1bc8a615568484f7145
Harold DAYE <dayeharold885@gmail.com>
2025-07-18 23:14:45 +0100

    dernière verification des fichiers

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import os 
-
+from os.path import exists
 #  Création du fichier cartes_reduction si nécessaire
 def creer_fichier_cartes_reduction():
     dossier = "./fichiers"
@@ -28,15 +28,7 @@ def creer_fichier_cartes_reduction():
 clients = pd.read_excel("./fichiers/Clients.xlsx",engine="openpyxl")
 produits = pd.read_excel("./fichiers/Produits.xlsx", engine="openpyxl")
 
-#function qui permet d'ecrire les clients dans le fichier clients.xlsx
-def ecrire_clients(nom, contact, code_client, IFU):
-    nouveau_client = pd.DataFrame({
-        "nom": [nom],
-        "contact": [contact],
-        "code_client": [code_client],
-        "IFU": [IFU],
-    })
-    return nouveau_client
+
     #function qui permet d'ecrire les produits dans le fichier produits.xlsx
 def ecrire_produits(code_produit, libelle, prix_unitaire):
     nouveau_produit = pd.DataFrame({
@@ -46,11 +38,6 @@ def ecrire_produits(code_produit, libelle, prix_unitaire):
     })
     return nouveau_produit
      
-   
-#function qui permet de generer un code client 
-def generer_code_client(nom, contact,IFU):
-    code = nom[:3].lower() + contact[:3].upper() + IFU[-3:]
-    return code
 
 #function qui permet de generer un code produit
 def generer_code_produit(libelle):
@@ -77,4 +64,58 @@ def valider_code(code_client,code_produit):
     client_existe = code_client in clients['code_client'].values
     produit_existe = code_produit in produits['code_produit'].values
     return client_existe and produit_existe
+    #creation du fichier cartes_reduction si nécessaire
+    
+# creer_fichier_cartes_reduction()
+# cartes = pd.read_excel("./fichiers/cartes_reduction.xlsx", engine="openpyxl")
+
+#function qui permet de generer un code client 
+def generer_code_client(nom, contact,IFU):
+    code = nom[:3].lower() + contact[:3].upper() + IFU[-3:]
+    return code
+
+#function qui permet d'ecrire les clients dans le fichier clients.xlsx
+def ecrire_clients(nom, contact, code_client, IFU):
+    nouveau_client = pd.DataFrame({
+        "nom": [nom],
+        "contact": [contact],
+        "code_client": [code_client],
+        "IFU": [IFU],
+    })
+    return nouveau_client
+    
+def ajouter_client(nom,contact,IFU,code_client,clients,produits,cartes):
+
+    # Chargement des fichiers
+    clients = pd.read_excel("./fichiers/Clients.xlsx", engine="openpyxl") \
+        if exists("./fichiers/Clients.xlsx") else pd.DataFrame(columns=["nom", "contact", "code_client", "IFU"])
+
+    produits = pd.read_excel("./fichiers/Produits.xlsx", engine="openpyxl") \
+        if exists("./fichiers/Produits.xlsx") else pd.DataFrame(columns=["code_produit", "libelle", "prix_unitaire"])
+
+    cartes = pd.read_excel("./fichiers/cartes_reduction.xlsx", engine="openpyxl")
+
+    
+ # Saisie des infos client
+    nom = input("Nom du client : ")
+    contact = input("Contact du client : ")
+    IFU = input("IFU du client : ")
+
+    # Traitement
+    code_client = generer_code_client(nom, contact, IFU)
+    nouveau_client = ecrire_clients(nom, contact, code_client, IFU)
+
+    # Ajout et sauvegarde
+    clients = pd.concat([clients, nouveau_client], ignore_index=True)
+    clients.to_excel("./fichiers/Clients.xlsx", index=False, engine="openpyxl")
+
+    # Confirmation
+    print(f"Client '{nom}' enregistré avec le code : {code_client}")
+
+
+
+    
+   
+
+
     
--- main.py
+++ main.py
@@ -1,31 +1,8 @@
 import pandas as pd
 from os.path import exists
-from data_manager import (
-    creer_fichier_cartes_reduction,
-    ecrire_clients,generer_code_client
-)
+from data_manager import (ajouter_client)
 from produit_manager import(ajouter_produit)
-#creation du fichier cartes_reduction si nécessaire
-creer_fichier_cartes_reduction()
-cartes = pd.read_excel("./fichiers/cartes_reduction.xlsx", engine="openpyxl")
 
-#  Chargement des fichiers Excel
-clients = pd.read_excel("./fichiers/Clients.xlsx", engine="openpyxl") \
-    if exists("./fichiers/Clients.xlsx") else pd.DataFrame(columns=["nom", "contact", "code_client", "IFU"])
-
-produits = pd.read_excel("./fichiers/Produits.xlsx", engine="openpyxl") \
-    if exists("./fichiers/Produits.xlsx") else pd.DataFrame(columns=["code_produit", "libelle", "prix_unitaire"])
-
-cartes = pd.read_excel("./fichiers/cartes_reduction.xlsx", engine="openpyxl")
-
-# Saisie des infos client
-nom = input("Nom du client : ")
-contact = input("Contact du client : ")
-IFU = input("IFU du client : ")
-code_client = generer_code_client(nom, contact, IFU)
-nouveau_client = ecrire_clients(nom, contact, code_client, IFU)
-clients = pd.concat([clients, nouveau_client], ignore_index=True)
-clients.to_excel("./fichiers/Clients.xlsx", index=False, engine="openpyxl")
-print(f"Client '{nom}' enregistré avec le code : {code_client}")
-
+ajouter_client('nom','contact','IFU','code_client','clients','produits','cartes',)
 ajouter_produit()
+
